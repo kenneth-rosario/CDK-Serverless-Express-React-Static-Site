f316a3e680f66e851506070875a366653357fbb4
kenneth-rosario <kenneth.rosario7@upr.edu>
2021-10-24 21:23:50 -0400

    Testing pipeline
    Fix

---
+++ infrastructure/lib/projects.ts
@@ -0,0 +1,67 @@
+import { BuildSpec, PipelineProject } from "@aws-cdk/aws-codebuild";
+import { Construct } from "@aws-cdk/core";
+
+
+export class ReactProject extends PipelineProject {
+    constructor(scope: Construct) {
+        super(scope, 'ReactProject', {
+            buildSpec: BuildSpec.fromObjectToYaml({
+                version: 0.2,
+                phases: {
+                    install: {
+                        runtime_versions: {
+                            nodejs: "14.x"
+                        },
+                        commands: [
+                            "cd ${CODEBUILD_SRC_DIR}/frontend && npm install && cd ..", // frontend install
+                        ]
+                    },
+                    build: {
+                        commands: [
+                            "cd ${CODEBUILD_SRC_DIR}/frontend && npm run build && cd .."
+                        ]
+                    },
+                    artifacts:{
+                        files: [
+                            "${CODEBUILD_SRC_DIR}/frontend/out/**/*"
+                        ]
+                    }
+                }
+            })
+        })
+    }
+}
+
+
+export class CdkProject extends PipelineProject {
+    constructor(scope: Construct) {
+        super(scope, 'CdkProject', {
+            buildSpec: BuildSpec.fromObjectToYaml({
+                version: 0.2,
+                phases: {
+                    install: {
+                        runtime_versions: {
+                            nodejs: "14.x"
+                        },
+                        commands: [
+                            "npm install && cd ${CODEBUILD_SRC_DIR}/backend && npm install && cd .."
+                        ]
+                    },
+                    build: {
+                        commands: [
+                            "cd ${CODEBUILD_SRC_DIR}",
+                            "npm run build",
+                            "npm run cdk synth"
+                        ]
+                    },
+                    artifacts:{
+                        "base-directory": "${CODEBUILD_SRC_DIR}",
+                        files: [
+                            "cdk.out/**/*",
+                        ]
+                    }
+                }
+            })
+        })
+    }
+}
---
+++ infrastructure/stacks/infra-stack.ts
@@ -0,0 +1,98 @@
+import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigateway from '@aws-cdk/aws-apigateway';
+import * as s3 from '@aws-cdk/aws-s3'
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+import {NodejsFunction} from '@aws-cdk/aws-lambda-nodejs';
+import * as path from 'path'
+import { SecretValue } from '@aws-cdk/core';
+import { CdkProject, ReactProject } from '../lib/projects';
+
+export class InfraStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const parentPath = path.join(process.cwd(), 'backend/src/serverless.ts')
+
+    const expressBackend = new NodejsFunction(this, 'express-backend', {
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(5),
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'handler',
+      entry: path.join(parentPath),
+    });
+    
+    const api = new apigateway.LambdaRestApi(this, 'backend', {
+      handler: expressBackend
+    })
+
+    // Static site
+    const staticS3 = new s3.Bucket(this, 'test-bucket', {
+      publicReadAccess: true,
+      websiteIndexDocument: 'index.html'
+    })
+
+    // Pipeline
+    const sourceArtifact = new codepipeline.Artifact();
+    const reactStaticSite = new codepipeline.Artifact();
+    const cdkOut = new codepipeline.Artifact();
+
+    const project = new ReactProject(this);
+    const cdkProject = new CdkProject(this);
+    
+    const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
+      // The pipeline name
+      pipelineName: 'FirstPipeline',
+      crossAccountKeys: false,
+      stages: [
+        {
+          stageName: "Source",
+          actions: [
+            new codepipeline_actions.GitHubSourceAction({
+              owner: "kenneth-rosario",
+              repo: "Notaso2.0",
+              oauthToken: SecretValue.secretsManager('github-token'),
+              actionName: "GithubRepo",
+              branch: 'main',
+              output: sourceArtifact
+            })
+          ]
+        },
+        {
+          stageName: "Build",
+          actions: [
+            new codepipeline_actions.CodeBuildAction({
+              input: sourceArtifact,
+              project: project,
+              actionName: "ReactBuild",
+              outputs: [reactStaticSite]
+            }),
+            new codepipeline_actions.CodeBuildAction({
+              input:sourceArtifact,
+              project: cdkProject,
+              actionName: "CDKBuild",
+              outputs: [cdkOut]
+            })  
+          ]
+        },
+        {
+          stageName: "Deploy",
+          actions: [
+            new codepipeline_actions.S3DeployAction({
+              actionName: "ReactDeploy",
+              input: reactStaticSite,
+              bucket: staticS3
+            }),
+            new codepipeline_actions.CloudFormationCreateUpdateStackAction({
+              actionName: "CdkDeploy",
+              templatePath: cdkOut.atPath("InfraStack.template.json"),
+              stackName: "InfraStack",
+              adminPermissions: true,
+            })
+          ]
+        }
+      ]
+    })
+  }
+}
