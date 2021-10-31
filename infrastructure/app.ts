61f7e8abc6087774317457da944996bc30424812
kenneth-rosario <kenneth.rosario7@upr.edu>
2021-10-30 22:28:10 -0400

    Forgot name

--- infrastructure/app.ts
+++ infrastructure/app.ts
@@ -6,7 +6,7 @@ import * as stacks from './stacks'
 const app = new cdk.App();
 
 // Complete infrastructure stack 
-new stacks.PipelineStack(app, 'InfraStack');
+new stacks.PipelineStack(app, 'PipelineStack');
 
 // Complete serverless backend stack
 new stacks.ServerlessStack(app, 'ServerlessBackend')
