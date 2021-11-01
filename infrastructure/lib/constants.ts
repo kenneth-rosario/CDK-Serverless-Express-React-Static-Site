1cafc22dc5f803b50120f3f0f0f1329a33e48881
kenneth-rosario <kenneth.rosario7@upr.edu>
2021-10-31 21:23:54 -0400

    added constant

--- infrastructure/lib/constants.ts
+++ infrastructure/lib/constants.ts
@@ -1,6 +1,6 @@
 import * as path from 'path'
 
-export const GITHUB_REPO = "kenneth-rosario/CDK-Serverless-Fullstack"
+export const GITHUB_REPO = "kenneth-rosario/CDK-Serverless-Express-React-Static-Site"
 export const SERVERLESS_BACKEND_OUT = path.join(process.cwd(), 'serverless-backend/bundle')
 export const STATIC_SITE_OUT = path.join(process.cwd(), 'frontend/out')
 
